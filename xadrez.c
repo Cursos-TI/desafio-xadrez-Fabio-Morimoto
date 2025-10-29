5a1cbbf9f8676746536169dd836ecf8f364e5e99
Fabio-Morimoto <fabio.morimoto19@gmail.com>
2025-10-29 23:24:34 +0000

    Criando a movimentação das peças usando loops

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,28 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int bispo = 0; // Diagonal superior direito
+    int torre = 0; // Se move para a direita
+    int rainha; // Se move para a esquerda
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    do {
+        printf("O bispo andou %d casa(s) para a diagonar superior direito.\n", bispo);
+        bispo++;
+    } while (bispo <= 5);
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+
+    while (torre <= 5) {
+        printf("A torre andou %d casa(s) para a direita\n", torre);
+        torre++;
+    };
+
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    for (rainha = 0; rainha <= 8; rainha++) {
+        printf("A rainha andou %d casa(s) para a esquerda.\n", rainha);
+    };
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
